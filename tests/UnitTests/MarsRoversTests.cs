4370d7d40e5b290f129d42239e5778951392bad0
amlsantos <6472330+santos-an@users.noreply.github.com>
2024-01-02 14:18:34 +0000

    red - move foward

--- tests/UnitTests/MarsRoversTests.cs
+++ tests/UnitTests/MarsRoversTests.cs
@@ -21,4 +21,20 @@ public class MarsRoversTests
         // assert
         result.Should().Be(expectedEndingPoint);
     }
+
+    [Theory]
+    [InlineData("0:0:N", "0:0:N")]
+    [InlineData("1:1:N", "1:2:N")]
+    public void Execute_FowardCommand_ReturnsCorrectPosition(string startingPoint, string expectedEndingPoint)
+    {
+        // arrange
+        var fowardCommand = "F";
+        var marsRovers = new MarsRovers(startingPoint);
+
+        // act
+        var result = marsRovers.Execute(fowardCommand);
+
+        // assert
+        result.Should().Be(expectedEndingPoint);
+    }
 }
