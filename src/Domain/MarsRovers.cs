89f2f9c88b788a77f8a3ec319dc27374044f88bf
amlsantos <6472330+santos-an@users.noreply.github.com>
2024-01-02 14:57:25 +0000

    refactor - extract methods

--- src/Domain/MarsRovers.cs
+++ src/Domain/MarsRovers.cs
@@ -19,16 +19,56 @@ public class MarsRovers
 
         if (command == "F")
         {
-            if (direction == "N")
-                return $"{x}:{y+1}:{direction}";
-            if (direction == "E")
-                return $"{x+1}:{y}:{direction}";
-            if (direction == "S")
-                return $"{x}:{y-1}:{direction}";
-            if (direction == "W")
-                return $"{x-1}:{y}:{direction}";
+            if (IsNorth(direction))
+                return MoveNorth(x, y, direction);
+            if (IsEast(direction))
+                return MoveEast(x, y, direction);
+            if (IsSouth(direction))
+                return MoveSouth(x, y, direction);
+            if (IsWest(direction))
+                return MoveWest(x, y, direction);
         }
 
         return string.Empty;
+    }    
+
+    private bool IsNorth(string direction)
+    {
+        return direction.Equals("N", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private string MoveNorth(int x, int y, string direction)
+    {
+        return $"{x}:{y + 1}:{direction}";
+    }
+
+    private bool IsEast(string direction)
+    {
+        return direction.Equals("E", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private string MoveEast(int x, int y, string direction)
+    {
+        return $"{x + 1}:{y}:{direction}";
+    }
+
+    private bool IsSouth(string direction)
+    {
+        return direction.Equals("S", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private string MoveSouth(int x, int y, string direction)
+    {
+        return $"{x}:{y - 1}:{direction}";
+    }
+
+    private bool IsWest(string direction)
+    {
+        return direction.Equals("W", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private string MoveWest(int x, int y, string direction)
+    {
+        return $"{x - 1}:{y}:{direction}";
     }
 }
